88d9d4c3f9b324d2253f2609afdeb04237cdc1b3
Peter Bruce <peter@researchunit.co.uk>
2018-11-11 18:59:52 +0000

    video test

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,19 @@
-var ticker=document.querySelector(".ticker"),list=document.querySelector(".ticker__list"),clone=list.cloneNode(!0);ticker.append(clone);
+document.addEventListener('DOMContentLoaded', () => {
+	// First real dialog starts around 1:47
+	const source = 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd';
+	// For more dash options, see https://github.com/Dash-Industry-Forum/dash.js
+
+	const dash = dashjs.MediaPlayer().create();
+	const video = document.querySelector('video');
+	dash.getDebug().setLogToBrowserConsole(false);
+	dash.initialize(video, source, true);
+
+	// Update caption tracks after initializing Plyr to get the generated captions
+	// For more options see: https://github.com/sampotts/plyr/#options
+	const player = new Plyr(video, {autoplay: true, playsinline: true, controls: [""], loop: { active: true }});
+
+	// Expose player and dash so they can be used from the console
+	window.player = player;
+	window.dash = dash;
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
-var ticker = document.querySelector('.ticker')
-  , list = document.querySelector('.ticker__list')
-  , clone = list.cloneNode(true)
+document.addEventListener('DOMContentLoaded', () => {
+	// First real dialog starts around 1:47
+	const source = 'https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd';
+	// For more dash options, see https://github.com/Dash-Industry-Forum/dash.js
 
-ticker.append(clone)
+	const dash = dashjs.MediaPlayer().create();
+	const video = document.querySelector('video');
+	dash.getDebug().setLogToBrowserConsole(false);
+	dash.initialize(video, source, true);
+
+	// Update caption tracks after initializing Plyr to get the generated captions
+	// For more options see: https://github.com/sampotts/plyr/#options
+	const player = new Plyr(video, {autoplay: true, playsinline: true, controls: [""], loop: { active: true }});
+
+	// Expose player and dash so they can be used from the console
+	window.player = player;
+	window.dash = dash;
+
+});

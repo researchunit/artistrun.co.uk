fe0def0bf881d86648b638d8c75b6c916e8b6e8b
Peter Bruce <peter@researchunit.co.uk>
2019-01-27 18:53:36 +0000

    remove video

--- docs/video-alia/js/main.js
+++
@@ -1,6 +0,0 @@
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-const player = new Plyr('video', {playsinline: true, autoplay: true, controls: [""], loop: { active: true }});
-
-// Expose player so it can be used from the console
-window.player = player;
